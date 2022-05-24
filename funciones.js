a3b2994dd8528d2bfbbb53a6525b5eec0d0cb0e2
dianaaltamirano <nathalyusl@gmail.com>
2022-05-23 21:05:58 -0500

    index creacion

---
+++ funciones.js
@@ -0,0 +1,8 @@
+function colores(valor){
+    if (valor==1){
+        document.getElementById('traduccion').innerHTML="Ankas";
+    }
+    if (valor==1){
+        document.getElementById('traduccion').innerHTML="Ankas";
+    }
+}
